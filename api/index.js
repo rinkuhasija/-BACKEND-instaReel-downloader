cf121c4f4617e10e58408be7d3194790c78648d5
Rinku Hasija <rishabhasija5454@gmail.com>
2025-04-15 20:04:00 +0530

    test

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // /api/instagram.js
-const instagramGetUrl = require('instagram-url-direct');
+import instagramGetUrl from 'instagram-url-direct';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -8,14 +8,18 @@ export default async function handler(req, res) {
 
   const { url } = req.body;
 
+  if (!url) {
+    return res.status(400).json({ message: 'Missing URL' });
+  }
+
   try {
     const result = await instagramGetUrl(url);
     if (result?.url_list?.length > 0) {
       return res.status(200).json({ videoUrl: result.url_list[0] });
     }
     res.status(404).json({ message: 'Video not found' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 }
